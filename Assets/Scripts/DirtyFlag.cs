67c4aa004edabb902c1736e45595b08ecadf5baa
abdelsalam.megahed <abdelsalam.megahed@gmail.com>
2019-12-10 21:46:06 +0200

    Dirty flag design pattern is implemented

---
+++ Assets/Scripts/DirtyFlag.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirtyFlag : MonoBehaviour
+{
+    private Transform parentObjectTransform;
+    public Transform childTransform;
+    private bool isChanged = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parentObjectTransform = GetComponent<Transform>();
+    }
+
+    //private Transform getObjectTransform()
+    //{
+    //    return parentObjectTransform;
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            parentObjectTransform.position = new Vector2(parentObjectTransform.position.x, (parentObjectTransform.position.y)+8f);
+            SetTheflag();
+            Debug.Log("Parent object has moved");
+
+            clearTheFlag();
+        }
+    }
+
+    private void clearTheFlag()
+    {
+        childTransform = parentObjectTransform;
+        Debug.Log("Child position has been updated");
+        isChanged = false;
+    }
+
+    private void SetTheflag()
+    {
+        isChanged = true; 
+    }
+}
